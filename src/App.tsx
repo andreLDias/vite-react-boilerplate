9edea7f21ab8d18eedf32ae57a71e9e82f41f167
andreldias <andreluizdiascustodio@gmail.com>
2025-08-14 17:05:00 -0300

    fix import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import './app.css'
 
 function App() {
   const [count, setCount] = useState(0)
